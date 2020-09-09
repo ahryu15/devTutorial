85531aa89555cd94f6841e090643251a606b8e96
ryoo <ahryu15@hotmail.com>
2020-09-09 10:24:05 +0900

    초기 커밋.

---
+++ devTutorial.Module.Web/WebModule.Designer.cs
@@ -0,0 +1,36 @@
+﻿namespace devTutorial.Module.Web {
+    partial class devTutorialAspNetModule {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // devTutorialAspNetModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(devTutorial.Module.devTutorialModule));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule));
+			this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule));
+        }
+
+        #endregion
+    }
+}
