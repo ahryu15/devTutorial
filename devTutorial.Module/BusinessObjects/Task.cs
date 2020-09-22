c77d5254a5c6ebc52663943ad45f3f305b3b3277
ryoo <ahryu15@hotmail.com>
2020-09-22 17:15:11 +0900

    Add an Action with Option Selection

--- devTutorial.Module/BusinessObjects/Task.cs
+++ devTutorial.Module/BusinessObjects/Task.cs
@@ -64,8 +64,11 @@ namespace devTutorial.Module.BusinessObjects
     
     public enum Priority
     {
+        [ImageName("State_Priority_Low")]
         Low = 0,
+        [ImageName("State_Priority_Normal")]
         Normal = 1,
+        [ImageName("State_Priority_High")]
         High = 2
     }
 }
