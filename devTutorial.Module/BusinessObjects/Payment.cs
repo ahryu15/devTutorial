9f6ee6d5d1b24e75d3d2f9981a0c8b8c28bf7192
ryoo <ahryu15@hotmail.com>
2020-10-08 17:00:20 +0900

    Make a Property Calculable

---
+++ devTutorial.Module/BusinessObjects/Payment.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace devTutorial.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [ImageName("BO_SaleItem")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Payment : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public Payment(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue(nameof(PersistentProperty), ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+        private double rate;
+        public double Rate
+        {
+            get
+            {
+                return rate;
+            }
+            set
+            {
+                if (SetPropertyValue(nameof(Rate), ref rate, value))
+                    OnChanged(nameof(Amount));
+            }
+        }
+        private double hours;
+        public double Hours
+        {
+            get
+            {
+                return hours;
+            }
+            set
+            {
+                if (SetPropertyValue(nameof(Hours), ref hours, value))
+                    OnChanged(nameof(Amount));
+            }
+        }
+        [PersistentAlias("Rate * Hours")]
+        public double Amount
+        {
+            get
+            {
+                object tempObject = EvaluateAlias(nameof(Amount));
+                if (tempObject != null)
+                {
+                    return (double)tempObject;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
